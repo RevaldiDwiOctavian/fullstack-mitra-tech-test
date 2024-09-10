91ad4d8e300b1c7d2d0b85124cfea5bb46962b49
Revaldi Dwi Octavian <revaldi.dwioctavian@tujuhsembilan.com>
2024-09-10 13:20:44 +0700

    add delete function

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import {NgForOf} from "@angular/common";
+import axios from "axios";
 
 export interface Task {
   id: number;
@@ -20,14 +21,32 @@ export interface Task {
 })
 export class TableComponent {
   @Input() dataSource: Task[] = [];
+  private apiUrl = 'http://localhost:8000/api';
+
+  async getTasks(): Promise<Task[]> {
+    try {
+      const response = await axios.get(`${this.apiUrl}/tasks`);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+      throw error;
+    }
+  }
+
 
   onUpdate(task: Task): void {
     console.log('Update:', task);
     // Implement update logic here
   }
 
-  onDelete(task: Task): void {
-    console.log('Delete:', task);
+  async onDelete(task: Task): Promise<any> {
+    try {
+      const response = await axios.delete(`${this.apiUrl}/tasks/${task.id}`);
+      return response.data;
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
+      throw error;
+    }
     // Implement delete logic here
   }
 }
